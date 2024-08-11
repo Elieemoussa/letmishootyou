8ed0f18d38713c27e3cbbe75bddbd2ada582353b
Elie <moussaelie497@gmail.com>
2024-08-11 16:17:04 +0300

    fixing

---
+++ js/loader.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    const loadingScreen = document.getElementById('loading-screen');
+    const content = document.getElementById('content');
+    
+    // Hide the loading screen and show the content
+    loadingScreen.style.display = 'none';
+    content.style.display = 'block';
+});
