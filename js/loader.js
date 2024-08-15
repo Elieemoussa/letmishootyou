37a888df77081e9c70d76b5221f1c1f58c52e2a3
Elie <moussaelie497@gmail.com>
2024-08-15 11:53:04 +0300

    updating

--- js/loader.js
+++ js/loader.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const loadingScreen = document.getElementById('loading-screen');
     const content = document.getElementById('content');
     
-    console.log(loadingScreen); // Should not be null
-    console.log(content); // Should not be null
+
     
     if (loadingScreen && content) {
         // Hide the loading screen and show the content
