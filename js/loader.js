f5e0aa338cc11acc5ea34eac872513748d9c398b
Elie <moussaelie497@gmail.com>
2024-08-19 10:32:02 +0300

    fixing the loader fade

--- js/loader.js
+++ js/loader.js
@@ -4,9 +4,8 @@ $(window).on('load', function() {
     /*------------------
         Preloader
     --------------------*/
-    $(".loader").fadeOut();
+   
     $("#preloder").delay(400).fadeOut("slow", function() {
-        // Show the content after the preloader fades out
-        // $("#content").fadeIn("slow");
+        $(".loader").fadeOut();
     });
 });
