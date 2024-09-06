3804c20192e08431a800004ea04c79748e042324
Elie <moussaelie497@gmail.com>
2024-09-06 11:15:57 +0300

    fixing the menu overflow

--- js/menuslide.js
+++ js/menuslide.js
@@ -1,9 +1,11 @@
 // Toggle menu open
 document.querySelector('.flo-header-mobile__menu-trigger').addEventListener('click', function() {
     document.body.classList.add('body--flo-mobile-menu-popup-open');
+    document.body.classList.add('body--flo-mobile-menu-popup-opened'); // Prevent body scroll
 });
 
 // Close menu
 document.querySelector('.flo-mobile-menu__close').addEventListener('click', function() {
     document.body.classList.remove('body--flo-mobile-menu-popup-open');
+    document.body.classList.remove('body--flo-mobile-menu-popup-opened'); // Restore body scroll
 });
