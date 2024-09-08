ad4f3d99d9afc29e753ce50795c3a5206f29fd04
Elie <moussaelie497@gmail.com>
2024-09-08 13:12:09 +0300

    fixing

--- js/loader.js
+++ js/loader.js
@@ -1,23 +1,20 @@
 'use strict';
 
 $(window).on('load', function() {
-    /*------------------
-        Preloader
-    --------------------*/
-   
+    // Delay the preloader fadeout and fade out loader
     $("#preloder").delay(400).fadeOut("slow", function() {
         $(".loader").fadeOut();
+
+        // After loader fades out, slowly change body opacity
+        $("#content").css("opacity", "1"); // Trigger fade-in on body
     });
 });
-
 
 const element = document.querySelector('.lazyload'); // Adjust the selector as needed
 element.classList.add('lazyloading');
 
-// Simulate an image load or event
+// Simulate an image load or event for lazyloading images
 setTimeout(() => {
     element.classList.remove('lazyloading');
     element.classList.add('lazyloaded');
-}, 1000); // Adjust the timing as needed
-
-
+}, 1000); // Adjust timing as needed
